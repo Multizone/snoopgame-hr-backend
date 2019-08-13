0b7ef0a2581a526f12ed425d9b1a24a68bfa7299
Multizone <danil.baranichenko0210@gmail.com>
2019-08-13 16:56:08 +0300

    Added jwt security package

---
+++ src/main/java/net/snoopgame/hr/security/JwtUserDetailsService.java
@@ -0,0 +1,30 @@
+package net.snoopgame.hr.security;
+
+import lombok.extern.slf4j.Slf4j;
+import net.snoopgame.hr.model.User;
+import net.snoopgame.hr.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public JwtUserDetailsService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userService.findByUserName(userName);
+
+        if(user == null )
+            throw new UsernameNotFoundException("In method loadUserByUserName, user: " + userName + " not found");
+
+        return null;
+    }
+}
---
+++ src/main/java/net/snoopgame/hr/security/jwt/JwtUser.java
@@ -0,0 +1,69 @@
+package net.snoopgame.hr.security.jwt;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class JwtUser implements UserDetails {
+
+    private final Long id;
+    private final String userName;
+    private final String lastName;
+    private final String middleName;
+    private final String password;
+    private final boolean enabled;
+    private final Date lastPasswordResetDate;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public JwtUser(Long id, String userName, String lastName, String middleName, String password, boolean enabled,
+                   Date resetDate, Collection<? extends GrantedAuthority> authorities){
+        this.id = id;
+        this.userName = userName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.password = password;
+        this.enabled = enabled;
+        this.lastPasswordResetDate = resetDate;
+        this.authorities = authorities;
+    }
+
+    @JsonIgnore
+    public Long getId(){ return id; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return authorities; }
+
+    @JsonIgnore
+    @Override
+    public String getPassword() { return password; }
+
+    @Override
+    public String getUsername() { return userName; }
+
+
+    public String getLastName() { return  lastName; }
+
+
+    public String getMiddleName() { return  middleName; }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() { return true;}
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() { return true;}
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() { return true;}
+
+    @Override
+    public boolean isEnabled() { return enabled; }
+
+    @JsonIgnore
+    public Date getLastPasswordResetDate() { return lastPasswordResetDate; }
+}
