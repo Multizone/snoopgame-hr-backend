1549add2dbb386a979a23628fa75b7675fc43a0f
Multizone <danil.baranichenko0210@gmail.com>
2019-08-13 20:34:10 +0300

    Added security http config

---
+++ src/main/java/net/snoopgame/hr/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package net.snoopgame.hr.config;
+
+import net.snoopgame.hr.security.jwt.JwtConfigurer;
+import net.snoopgame.hr.security.jwt.JwtTokenFilter;
+import net.snoopgame.hr.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final String ADMIN_ENDPOINT = "/api/admin/**";
+    private static final String HR_ENDPOINT = "/api/hr/**";
+    private static final String LOGIN_ENDPOINT = "/api/auth/login";
+
+    @Autowired
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider){
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(LOGIN_ENDPOINT).permitAll()
+                .antMatchers(HR_ENDPOINT).hasRole("HR")
+                .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+}
