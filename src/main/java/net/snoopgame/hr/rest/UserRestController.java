8ba9a3cee9905cfb3242f9cc6a6fefd0965712fa
Multizone <danil.baranichenko0210@gmail.com>
2019-08-14 18:36:42 +0300

    Added UserRestController

---
+++ src/main/java/net/snoopgame/hr/rest/UserRestController.java
@@ -0,0 +1,35 @@
+package net.snoopgame.hr.rest;
+
+import net.snoopgame.hr.dto.UserDto;
+import net.snoopgame.hr.model.User;
+import net.snoopgame.hr.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/api/user/")
+public class UserRestController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id){
+        User user = userService.findById(id);
+
+        if(user == null)
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        UserDto result = UserDto.fromUser(user);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
